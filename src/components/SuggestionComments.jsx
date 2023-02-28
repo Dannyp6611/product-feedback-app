9c3e3933bc75dcaea4a40d90eacd35ae9f5593e4
Danny Parker <danielparker96@hotmail.co.uk>
2023-02-28 15:54:47 +0000

    added reply toggle to comment section

--- src/components/SuggestionComments.jsx
+++ src/components/SuggestionComments.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useUserContext } from '../context/useUserContext';
 
 const SuggestionComments = ({ comments }) => {
+  const { user } = useUserContext();
+
+  const [newReply, setNewReply] = useState('');
+  const [toggling, setToggling] = useState({
+    commentID: null,
+    isToggling: false,
+  });
+
+  const handleReplyToggle = (id) => {
+    setToggling({ commentID: id, isToggling: true });
+  };
+
+  const handleSubmit = (e, commentData) => {
+    e.preventDefault();
+
+    // create new reply
+    const replyToAdd = {
+      content: newReply,
+      replyingTo: commentData.user.username,
+      user: {
+        image: user.photoURL,
+        username: user.displayName,
+      },
+    };
+
+    console.log(replyToAdd);
+  };
+
   return (
-    <div className="bg-white rounded-md p-4">
+    <div className="bg-white rounded-md p-6">
       {/* comments title */}
       <h2 className="text-2xl font-bold text-grayPrimary mb-4">
         {comments.length} comments
@@ -10,13 +40,55 @@ const SuggestionComments = ({ comments }) => {
       {/* comments */}
       <div className="flex flex-col gap-y-4">
         {comments.map((comment) => (
-          <div key={comment.id} className="border-b border-gray-200 py-3">
-            <img
-              src={comment.user.image}
-              width="40"
-              height="40"
-              className="rounded-full"
-            />
+          <div className="border-b border-gray-200 py-3">
+            <div key={comment.id} className="md:flex gap-4 relative">
+              <button
+                onClick={() => handleReplyToggle(comment.id)}
+                className="text-colorBluePrimary hover:underline underline-offset-1 font-bold absolute right-0 text-sm"
+              >
+                Reply
+              </button>
+              <div className="flex items-center">
+                <div className="w-[40px] h-[40px] mr-[4px]">
+                  <img
+                    src={comment.user.image}
+                    width="40"
+                    height="40"
+                    className="object-cover rounded-full"
+                  />
+                </div>
+                <h4 className="text-sm text-grayPrimary font-bold md:hidden">
+                  {comment.user.username}
+                </h4>
+              </div>
+              <div className="">
+                <h4 className="text-base text-grayPrimary font-bold hidden md:block">
+                  {comment.user.username}
+                </h4>
+                <p className="text-gray-600">{comment.content}</p>
+              </div>
+            </div>
+
+            {toggling.commentID === comment.id && toggling.isToggling && (
+              <form
+                onSubmit={(e) => handleSubmit(e, comment)}
+                className="text-6xl md:pl-[50px] mt-10 mb-4 flex flex-col md:flex-row gap-4 h-32 justify-items-start"
+              >
+                {/* buttons */}
+                <textarea
+                  value={newReply}
+                  onChange={(e) => setNewReply(e.target.value)}
+                  className="block rounded-md bg-gray-200 p-2 outline-none focus:ring-2 focus:ring-offset-1 focus:ring-colorPurple h-14 overflow-hidden flex-1 text-sm"
+                  placeholder={`replying to @${comment.user.username}`}
+                />
+                <div className="flex justify-between md:flex-col">
+                  {/* <button className="btn-primary">Post Reply</button> */}
+                  <button className="btn-primary" type="submit">
+                    Post Reply
+                  </button>
+                </div>
+              </form>
+            )}
           </div>
         ))}
       </div>
--- src/pages/SuggestionDetail.jsx
+++ src/pages/SuggestionDetail.jsx
@@ -31,7 +31,7 @@ const SuggestionDetail = () => {
   }
 
   return (
-    <div className="max-w-2xl mx-auto p-4 flex flex-col gap-y-4">
+    <div className="max-w-[330px] md:max-w-xl mx-auto p-4 flex flex-col gap-y-4">
       <div className="flex items-center justify-between">
         <Link to="/">Go Back</Link>
 
